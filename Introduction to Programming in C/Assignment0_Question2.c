4ea06c2033d85c9edd7851770157d8fafff0882d
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-06 17:00:09 +0530

    Create Assignment0_Question2.c

---
+++ Introduction to Programming in C/Assignment0_Question2.c
@@ -0,0 +1,11 @@
+/* Input: Two even integers.
+Output: Average of those numbers. */
+#include <stdio.h>
+int main(){
+  int a,b;
+  int c;
+  scanf("%d%d",&a,&b);
+  c=(int)(a+b)/2;
+  printf("%d",c);
+  return 0;
+}
