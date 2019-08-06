58b03642d0edfdab75b17fdde1360f35faa49827
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-06 16:52:01 +0530

    Create Assignments1_Addition.py

---
+++ The Joy of Computing using Python/Assignments1_Addition.py
@@ -0,0 +1,5 @@
+#In this assignment, you will have to take two numbers (integers) as input and print the addition.
+#The first line of the input contains two numbers separated by a space.
+#Print the addition in single line
+x, y = map(int, input().split())
+print(x+y)
