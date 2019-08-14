b82416bbd5fe5c66a1e6344cf11ff484c0416590
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-14 16:45:04 +0530

    Update Week3Program4.c

--- Problem Solving through Programming in C/Week3Program4.c
+++ Problem Solving through Programming in C/Week3Program4.c
@@ -1,17 +1,15 @@
-//Write a C program to check if a given Number is zero or Positive or Negative Using if...else statement.
+/* Write a C Program to calculates the area 
+(floating point number with two decimal places) of a Circle given it’s radius (integer value).  The value of Pi is 3.14. */
 #include <stdio.h>
-int main()
+#define PI 3.14
+void main()
 {
-    double number;
-    scanf("%lf", &number); 
-}
-if(number==0){
-  printf("The number is 0.");
-}
-else if(number>0){
-  printf("Positive number.");
-}
-else{
-  printf("Negative number.");
-}
+    int radius;
+    float area;
+    /* Enter the radius of a circle */
+    /* The radius of the circle is taken from the test cases */ 
+   
+    scanf("%d", &radius);
+    area=(float)PI*radius*radius;
+    printf("Area of a circle = %5.2f\n", area);
 }
