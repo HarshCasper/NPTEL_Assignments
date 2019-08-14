c5c2ebfad3f9f591ab0def8ab77548ffaf3edb95
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-14 16:43:29 +0530

    Create Week3Program4.c

---
+++ Problem Solving through Programming in C/Week3Program4.c
@@ -0,0 +1,17 @@
+//Write a C program to check if a given Number is zero or Positive or Negative Using if...else statement.
+#include <stdio.h>
+int main()
+{
+    double number;
+    scanf("%lf", &number); 
+}
+if(number==0){
+  printf("The number is 0.");
+}
+else if(number>0){
+  printf("Positive number.");
+}
+else{
+  printf("Negative number.");
+}
+}
