2f71eec11c5b5b8840d92436dd29024ef606bc1f
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-14 16:35:24 +0530

    Create Week3Program1.c

---
+++ Problem Solving through Programming in C/Week3Program1.c
@@ -0,0 +1,14 @@
+//Write a C program to check whether a given number (integer) is Even or Odd.
+#include <stdio.h>
+int main()
+{
+    int number;
+    scanf("%d", &number); /*An integer number is taken from the test case */
+}
+if(number%2==0){
+  printf("%d is even.", number);
+}
+else{
+  printf("%d is odd.", number);
+}
+}
