d6f634d8138ed725929a5f505116949e79d51645
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-08-17 13:17:55 +0530

    Update Assignments7_Factorial.py

--- The Joy of Computing using Python/Assignments7_Factorial.py
+++ The Joy of Computing using Python/Assignments7_Factorial.py
@@ -1,4 +1,4 @@
-#Given an integer number n, you have to print the factorial of this number. To know about factorial please click on this link.
+#Given an integer number n, you have to print the factorial of this number. 
 num=int(input())
 fact=1
 for i in range(1,num+1):
