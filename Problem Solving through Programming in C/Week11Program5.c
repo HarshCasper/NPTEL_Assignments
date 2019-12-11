9572738aa2a70a68410204d7ef4bcf5aa7257d4a
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2019-12-11 22:07:35 +0530

    Create Week11Program5.c

---
+++ Problem Solving through Programming in C/Week11Program5.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#define MAX 100
+char *reverse(char[]);
+
+int main()
+{
+    char str[MAX], *rev;
+    //printf("Enter a String: ");
+    scanf("%s", str);
+    rev = reverse(str); //You have to write this function
+    printf("The reversed string is : %s\n", rev);
+    return 0;
+}
+
+char *reverse(char str[]){
+  static int i=0;
+  static char rev[100];
+  if(*str){
+    reverse(str+1);
+    rev[i++]=*str;
+  }
+  return rev;
+}
