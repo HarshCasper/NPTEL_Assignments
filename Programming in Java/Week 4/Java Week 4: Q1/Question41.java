f2e8cc4cd7cdb10924760410e6c220474c77c164
Harsh Bardhan Mishra <47351025+HarshCasper@users.noreply.github.com>
2020-02-20 19:59:22 +0530

    Create Question41.java

---
+++ Programming in Java/Week 4/Java Week 4: Q1/Question41.java
@@ -0,0 +1,21 @@
+/*
+
+Complete the code segment to execute the following program successfully. You should import the correct package(s) and/or class(s) to complete the code.
+
+*/
+
+
+import static java.lang.System.out;
+import java.util.Scanner;
+
+// main class Question is created
+public class Question41{
+  public static void main(String[] args) {
+	// Scanner object is created
+    Scanner scanner = new Scanner(System.in);
+     // Read String input using scanner class
+    String courseName = scanner.nextLine(); 
+     // Print the scanned String
+    out.println("Course: " + courseName); 
+  }
+}
